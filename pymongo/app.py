a35e0bc39563bb6872528e3b0ac961ab2756375c
Diego <diego.eduardo.w@gmail.com>
2023-04-05 12:02:32 -0300

    Adicionando Projeto

---
+++ pymongo/app.py
@@ -0,0 +1,86 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+lista_produtos = [
+    {"produto": "Smartphone Samsung Galaxy S21 Ultra", "descricao": "O melhor smartphone da Samsung com câmera de alta resolução e tela AMOLED.", "categoria": "Eletrônicos"},    
+    {"produto": "Notebook Dell XPS 13", "descricao": "Um dos melhores notebooks no mercado com alta performance e tela InfinityEdge.", "categoria": "Informática"},    
+    {"produto": "Smartwatch Apple Watch Series 6", "descricao": "Um dos melhores smartwatches no mercado com monitoramento de saúde avançado e tela sempre ligada.", "categoria": "Eletrônicos"},    {"produto": "Tablet Apple iPad Pro", "descricao": "O melhor tablet da Apple com tela de alta resolução e processador A12Z Bionic.", "categoria": "Eletrônicos"},    
+    {"produto": "Fone de Ouvido Sony WH-1000XM4", "descricao": "Um dos melhores fones de ouvido no mercado com cancelamento de ruído e alta qualidade de som.", "categoria": "Informática"},    
+    {"produto": "Smart TV LG OLED CX", "descricao": "A melhor Smart TV da LG com resolução 4K e tecnologia OLED.", "categoria": "Televisores"},
+    {"produto": "Câmera Fotográfica Sony A7 III", "descricao": "Uma das melhores câmeras no mercado para fotografia profissional com resolução 4K.", "categoria": "eletrônicos"},    
+    {"produto": "Monitor Gamer Alienware 34 AW3420DW", "descricao": "O melhor monitor para jogos no mercado com resolução ultrawide e taxa de atualização de 120Hz.", "categoria": "eletrônicos"},    {"produto": "Impressora Multifuncional HP Laserjet Pro MFP M428fdw", "descricao": "Uma das melhores impressoras multifuncionais no mercado com alta velocidade de impressão e conectividade Wi-Fi.", "categoria": "eletrônicos"},    
+    {"produto": "Roteador Wi-Fi Asus RT-AC68U", "descricao": "Um dos melhores roteadores Wi-Fi no mercado com alta velocidade de conexão e segurança avançada.", "categoria": "eletrônicos"},    
+    {"produto": "Caixa de Som JBL Xtreme 3", "descricao": "Uma das melhores caixas de som portáteis no mercado com alta qualidade de som e resistência à água.", "categoria": "eletrônicos"},    
+    {"produto": "Console PlayStation 5", "descricao": "O mais novo console da Sony com resolução 4K e suporte a tecnologia Ray Tracing.", "categoria": "eletrônicos"},    
+    {"produto": "Teclado Gamer Razer Huntsman Elite", "descricao": "Um dos melhores teclados para jogos no mercado com teclas mecânicas e iluminação RGB.", "categoria": "eletrônicos"},    
+    {"produto": "Mouse Gamer Logitech G502 HERO", "descricao": "Um dos melhores mouses para jogos no mercado com alta precisão e iluminação RGB.", "categoria": "eletrônicos"},    
+    {"produto": "Headset Gamer HyperX Cloud Alpha", "descricao": "Um dos melhores headsets para jogos no mercado com alta qualidade de som e microfone destacável.", "categoria": "eletrônicos"},    {"produto": "Webcam Logitech StreamCam", "descricao": "Uma das melhores webcams no mercado com resolução Full HD e qualidade de imagem superior.", "categoria": "eletrônicos"},
+    {"produto": "Drone DJI Mavic Air 2", "descricao": "Um dos melhores drones no mercado com câmera de alta resolução e autonomia de voo de até 34 minutos.", "categoria": "tecnologia"},    
+    {"produto": "Cadeira Gamer DXRacer Formula", "descricao": "Uma das melhores cadeiras para jogos no mercado com design ergonômico e ajuste de altura e inclinação.", "categoria": "móveis"},    
+    {"produto": "Placa de Vídeo Nvidia GeForce RTX 3080", "descricao": "Uma das melhores placas de vídeo no mercado para jogos e renderização com tecnologia Ray Tracing e memória GDDR6X de alta velocidade", "categoria": "tecnologia"},    
+    {"produto": "Smart Speaker Amazon Echo Dot", "descricao": "Um dos melhores smart speakers no mercado com assistente virtual Alexa e integração com diversos dispositivos.", "categoria": "tecnologia"},    {"produto": "Smart Thermostat Nest Learning", "descricao": "Um dos melhores termostatos inteligentes no mercado com aprendizado automático e controle remoto pelo celular.", "categoria": "casa"},    {"produto": "Smart Lock August Wi-Fi Smart Lock", "descricao": "Uma das melhores fechaduras inteligentes no mercado com controle remoto pelo celular e integração com assistentes virtuais.", "categoria": "casa"},    
+    {"produto": "Smart Scale Withings Body+", "descricao": "Uma das melhores balanças inteligentes no mercado com monitoramento de saúde avançado e conexão com app no celular.", "categoria": "saúde"},    {"produto": "Smart Garden Click and Grow", "descricao": "Uma das melhores hortas inteligentes no mercado com cultivo automático e conexão com app no celular.", "categoria": "casa"},    
+    {"produto": "Drone DJI Phantom 4 Pro", "descricao": "Um dos melhores drones no mercado com câmera de alta resolução e estabilização de imagem avançada.", "categoria": "tecnologia"},    
+    {"produto": "Laptop Lenovo ThinkPad X1 Carbon", "descricao": "Um dos melhores laptops corporativos no mercado com alta performance e durabilidade.", "categoria": "tecnologia"},    
+    {"produto": "Smart Lock Schlage Encode", "descricao": "Uma das melhores fechaduras inteligentes no mercado com conectividade Wi-Fi e controle remoto pelo celular.", "categoria": "casa"},    
+    {"produto": "Smart Speaker Sonos One", "descricao": "Um dos melhores smart speakers no mercado com alta qualidade de som e integração com assistentes virtuais.", "categoria": "tecnologia"},
+    {"produto": "Smart Display Google Nest Hub Max", "descricao": "Um dos melhores smart displays no mercado com tela grande e integração com assistente virtual Google Assistant.", "categoria": "tecnologia"},    
+    {"produto": "Smartwatch Garmin Forerunner 945", "descricao": "Um dos melhores smartwatches para corrida no mercado com monitoramento avançado de atividade física e GPS.", "categoria": "saúde"},    {"produto": "Soundbar Sonos Arc", "descricao": "Uma das melhores soundbars no mercado com alta qualidade de som e tecnologia Dolby Atmos.", "categoria": "áudio"},    
+    {"produto": "Câmera de Segurança Arlo Pro 4", "descricao": "Uma das melhores câmeras de segurança no mercado com resolução 2K e conectividade Wi-Fi.", "categoria": "segurança"},    
+    {"produto": "Monitor LG Ultrafine 5K", "descricao": "Um dos melhores monitores no mercado com resolução 5K e conectividade Thunderbolt 3.", "categoria": "tecnologia"},    
+    {"produto": "Smart Lock Yale Assure Lock", "descricao": "Uma das melhores fechaduras inteligentes no mercado com controle remoto pelo celular e integração com assistentes virtuais.", "categoria": "casa"},    
+    {"produto": "Smart Speaker Bose Home Speaker 500", "descricao": "Um dos melhores smart speakers no mercado com alta qualidade de som e integração com assistentes virtuais.", "categoria": "áudio"},    {"produto": "Smartwatch Samsung Galaxy Watch 3", "descricao": "Um dos melhores smartwatches no mercado com monitoramento de saúde avançado e conectividade LTE.", "categoria": "saúde"},    
+    {"produto": "Laptop Apple MacBook Pro", "descricao": "Um dos melhores laptops no mercado com tela Retina e processador de última geração.", "categoria": "tecnologia"},    
+    {"produto": "Câmera Mirrorless Fujifilm X-T4", "descricao": "Uma das melhores câmeras no mercado para fotografia e vídeo com resolução 4K e estabilização de imagem no corpo.", "categoria": "fotografia"},    
+    {"produto": "HD Externo Seagate Backup Plus", "descricao": "Um dos melhores HDs externos no mercado com alta capacidade de armazenamento e velocidade de transferência de dados.", "categoria": "tecnologia"},    
+    {"produto": "Monitor Dell UltraSharp U2719D", "descricao": "Um dos melhores monitores no mercado com resolução QHD e ampla gama de cores.", "categoria": "tecnologia"}, 
+    {"produto": "Monitor Dell UltraSharp U2719D", "descricao": "Um dos melhores monitores no mercado com resolução QHD e ampla gama de cores.", "categoria": "Monitores"}, 
+    {"produto": "Impressora Fotográfica Canon Selphy CP1300", "descricao": "Uma das melhores impressoras fotográficas portáteis no mercado com qualidade de imagem superior.", "categoria": "Impressoras"}, 
+    {"produto": "Projetor Epson Home Cinema 5050UB", "descricao": "Um dos melhores projetores para home theater no mercado com resolução 4K e tecnologia HDR.", "categoria": "Projetores"}, 
+    {"produto": "Caixa de Som Bluetooth Bose SoundLink Revolve+", "descricao": "Uma das melhores caixas de som Bluetooth portáteis no mercado com som em 360 graus e resistência à água.", "categoria": "Caixas de Som"},
+    {"produto": "Console Xbox Series X", "descricao": "O mais novo console da Microsoft com resolução 4K e suporte a tecnologia Ray Tracing.", "categoria": "Consoles"}, 
+    {"produto": "Teclado Mecânico Corsair K95 RGB Platinum XT", "descricao": "Um dos melhores teclados mecânicos no mercado com teclas programáveis e iluminação RGB.", "categoria": "Teclados"}, 
+    {"produto": "Mouse sem fio Microsoft Surface", "descricao": "Um dos melhores mouses sem fio no mercado com alta precisão e design ergonômico.", "categoria": "Mouses"}, 
+    {"produto": "Headset Bose QuietComfort 35 II", "descricao": "Um dos melhores headsets com cancelamento de ruído no mercado com alta qualidade de som e conforto.", "categoria": "Headsets"}, 
+    {"produto": "Câmera de Segurança Arlo Pro 4", "descricao": "Uma das melhores câmeras de segurança no mercado com resolução 2K e visão noturna em cores.", "categoria": "Câmeras de Segurança"}, 
+    {"produto": "Smart Speaker Amazon Echo Studio", "descricao": "Um dos melhores smart speakers no mercado com som de alta qualidade e compatibilidade com assistentes virtuais.", "categoria": "Smart Speakers"}, 
+    {"produto": "Aspirador de Pó Robô iRobot Roomba i7+", "descricao": "Um dos melhores aspiradores de pó robô no mercado com mapeamento inteligente e esvaziamento automático do coletor.", "categoria": "Aspiradores de Pó"}
+] 
+
+
+lista_nomes = [
+    "João Carlos",
+    "Maria Carla",
+    "José Fagunes",
+    "Ana Rote",
+    "Pedro Lacre",
+    "Mariana Jorna",
+    "Fernando Madonatoz",
+    "Carla Rocaria",
+    "Ricardo Lara",
+    "Camila Crita",
+    "Gabriel Jesus",
+    "Luana Pedreira",
+    "André Fortenites",
+    "Aline Carlinhas",
+    "Bruno Castro",
+    "Isabela Romaria",
+    "Leonardo Pereo",
+    "Julia Carlota",
+    "Lucas Forte",
+    "Bianca Nunes",
+    "Felipe Caio",
+    "Mônica Krita",
+    "Guilherme Loki",
+    "Larissa Taiga",
+    "Thiago Carlos"
+]
+
+@app.get("/lista_produtos")
+async def get_lista_produtos():
+    return {"lista_produtos": lista_produtos}
+
+@app.get("/lista_nomes")
+def get_nomes():
+    return {"lista_nomes": lista_nomes}
---
+++ pymongo/main.py
@@ -0,0 +1,40 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import random
+from datetime import date, datetime
+from tqdm import tqdm
+import requests
+from fastapi import FastAPI
+import pandas as pd
+
+
+app = FastAPI()
+
+uri = "mongodb://localhost:27017"
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client['vendas']
+coluna = db['vendas']
+
+data_aleatoria = pd.date_range(start="2000-01-01", end=datetime.today()).to_list()
+
+response_nomes = requests.get(f"http://{app.host}:{app.port}/lista_nomes")
+lista_nomes = response_nomes.json()["lista_nomes"]
+
+response_produtos = requests.get(f"http://{app.host}:{app.port}/lista_produtos")
+lista_produtos = response_produtos.json()["lista_produtos"]
+
+for _ in tqdm(range(1_000_000)):
+    dicio = {
+        'cod_venda': random.randint(1, 5000),
+        'data_teste': str(random.choice(data_aleatoria)),
+        'cod_cliente': random.randint(1, 5000),
+        'nome_cliente': random.choice(lista_nomes),
+        'cod_produto': random.randint(1, 5000),
+        'descricao': random.choice([produto['descricao'] for produto in lista_produtos]),
+        'nome_produto': random.choice(lista_produtos),
+        'categoria': random.choice([categoria['categoria'] for categoria in lista_produtos]),
+        'valor': round(random.uniform(1, 150), 2),
+        'qtd': random.randint(1, 11)
+    }
+   
+    coluna.insert_one(dicio)
